253d620c8476a80ca4942a92293e25269f76cb38
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-11 23:48:40 +0300

    2 homework ready

---
+++ task01/Program.cs
@@ -0,0 +1,33 @@
+﻿System.Console.WriteLine("Впишите 3 числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max;
+if (number1 > number2) {
+    max = number1;
+    if (number1 > number3) {
+        System.Console.WriteLine("Самое большое число: ");
+        System.Console.WriteLine(max);
+        
+    }
+}
+
+
+if (number1 < number2) {
+    max = number2;
+    if (number2 > number3) {
+        System.Console.WriteLine("Самое большое число: ");
+        System.Console.WriteLine(max);
+        
+    }
+}
+
+if (number3 > number1) {
+    max = number3;
+    if (number2 < number3) {
+        System.Console.WriteLine("Самое большое число: ");
+        System.Console.WriteLine(max);
+
+        
+    }
+}
